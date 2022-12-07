17bc4ba584a60fc76e3ea0af49126e50c4a7ca8c
kota-aso <kotaaso1212@gmail.com>
2022-12-07 15:33:00 +0900

    Initial commit

---
+++ src/main/java/in/techcamp/issueapp38535/Issueapp38535Application.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp38535;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issueapp38535Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Issueapp38535Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/issueapp38535/Issueapp38535ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp38535;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issueapp38535ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
